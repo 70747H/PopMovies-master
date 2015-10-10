50466b098ab931e7de469b3d03cbec81aa642abc
bold <hussien.fcih2016@gmail.com>
2015-10-10 22:48:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hussien/popmovies/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.hussien.popmovies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hussien.popmovies.model.Movie;
+
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.Callback {
+
+    private boolean mTab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.movie_detail_container) != null) {
+            mTab = true;
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.movie_detail_container, new DetailActivityFragment(),
+                                DetailActivityFragment.TAG)
+                        .commit();
+            }
+        } else {
+            mTab = false;
+        }
+    }
+
+    @Override
+    public void onItemSelected(Movie movie) {
+        if (mTab) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(DetailActivityFragment.DETAIL_MOVIE, movie);
+
+            DetailActivityFragment fragment = new DetailActivityFragment();
+            fragment.setArguments(arguments);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.movie_detail_container, fragment, DetailActivityFragment.TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class)
+                    .putExtra(DetailActivityFragment.DETAIL_MOVIE, movie);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hussien/popmovies/Utility.java
@@ -0,0 +1,53 @@
+package com.example.hussien.popmovies;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.hussien.popmovies.data.MovieContract;
+
+/**
+ * Created by Hussien on 31-Aug-15.
+ */
+public class Utility {
+
+    public static int isFavorited(Context context, int id) {
+        Cursor cursor = context.getContentResolver().query(
+                MovieContract.MovieEntry.CONTENT_URI,
+                null,
+                MovieContract.MovieEntry.COLUMN_MOVIE_ID + " = ?",
+                new String[] { Integer.toString(id) },
+                null
+        );
+        int numRows = cursor.getCount();
+        cursor.close();
+        return numRows;
+    }
+
+    public static String buildImageUrl(int width, String fileName) {
+        return "http://image.tmdb.org/t/p/w" + Integer.toString(width) + fileName;
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+}
